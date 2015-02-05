381c7810bd22260a0d32152c59456f8938f73451
ilius33 <ilius33@mail.ru>
2015-02-05 17:06:16 +0300

    Almost done with the first task

---
+++ script.js
@@ -0,0 +1,24 @@
+angular.module('ToDoList', [])
+  .controller('ToDoListController', ['$scope', function($scope) {
+    $scope.tasks = [
+      {text:"very long text first task already done very long text first task already done", done:true},
+      {text:"second task", done:false}
+    ];
+
+    $scope.remaining = function() {
+      var count = 0;
+      angular.forEach($scope.tasks, function(task) {
+        if (!task.done) count++;
+      });
+      return count;
+    };    
+
+    $scope.completeTask = function($event, task){
+      task.done=!task.done;
+    };
+
+    $scope.saveTask = function () {
+      $scope.tasks.push({text:this.new_task,done:false});
+    };
+
+  }]);
